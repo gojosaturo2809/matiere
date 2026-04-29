fae7c0d1847fbf992a96920ecd94d04e17849d7b
Herimino5 <heriminoandr5@gmail.com>
2026-04-29 10:18:29 +0300

    table etu

---
+++ app/Database/Migrations/herimino29-04-26(2).sql
@@ -0,0 +1,10 @@
+CREATE TABLE Etudiant (
+    id_etudiant INT PRIMARY KEY AUTO_INCREMENT,
+    ETU INT NOT NULL UNIQUE, -- Numéro matricule
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE,
+    id_parcours INT, -- Pour la liaison avec les parcours[cite: 3]
+    date_inscription DATE DEFAULT (CURRENT_DATE),
+    FOREIGN KEY (id_parcours) REFERENCES Parcours(id_parcours)
+);
