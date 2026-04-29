600d2f8e31ee6159bc168d0f3bd4af77bc707c46
Herimino5 <heriminoandr5@gmail.com>
2026-04-29 10:46:43 +0300

    Listeetu[1]

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,14 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Routes pour la Liste des Étudiants
+$routes->get('/etudiants', 'Etudiant::index');  // Liste des étudiants
+
+// Les routes suivantes seront implémentées par les autres membres du groupe:
+// $routes->get('/etudiants/create', 'Etudiant::create');
+// $routes->post('/etudiants', 'Etudiant::store');
+// $routes->get('/etudiants/(:num)', 'Etudiant::show/$1');
+// $routes->get('/etudiants/(:num)/edit', 'Etudiant::edit/$1');
+// $routes->put('/etudiants/(:num)', 'Etudiant::update/$1');
+// $routes->delete('/etudiants/(:num)', 'Etudiant::delete/$1');
---
+++ app/Controllers/Etudiant.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Etudiant as EtudiantModel;
+
+class Etudiant extends BaseController
+{
+    protected $etudiantModel;
+
+    public function __construct()
+    {
+        $this->etudiantModel = new EtudiantModel();
+    }
+
+    /**
+     * Afficher la liste de tous les étudiants
+     */
+    public function index()
+    {
+        $data = [
+            'etudiants' => $this->etudiantModel->findAll(),
+        ];
+
+        return view('etudiants/list', $data);
+    }
+
+    // Les méthodes suivantes seront implémentées par les autres membres du groupe:
+    // - create() : Afficher le formulaire de création
+    // - store() : Enregistrer un nouvel étudiant
+    // - show() : Afficher les détails et les notes d'un étudiant
+    // - edit() : Afficher le formulaire d'édition
+    // - update() : Mettre à jour un étudiant
+    // - delete() : Supprimer un étudiant
+}
